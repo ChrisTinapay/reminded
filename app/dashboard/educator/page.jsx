d6f713a5fe04ff5c7c811d9ed9040530625a069f
PC 4 <rgonzaga25a-0065@olopsc.ph>
2025-11-13 00:17:50 -0800

    Draft of dashboards

---
+++ app/dashboard/educator/page.jsx
@@ -0,0 +1,9 @@
+// app/dashboard/educator/page.jsx
+export default function EducatorDashboard() {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold">My Educator Dashboard</h1>
+        <p>My uploaded materials and classes will go here.</p>
+      </div>
+    )
+  }
