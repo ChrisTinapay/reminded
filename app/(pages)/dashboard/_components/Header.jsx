32aec794a1eac7948feb66cfa27c5b173796e7c1
GadainoC <cgadiano22a-0029@olopsc.edu.ph>
2025-11-29 23:14:19 +0800

    change sidebar, layout and styling

--- app/(pages)/dashboard/_components/Header.jsx
+++ app/(pages)/dashboard/_components/Header.jsx
@@ -32,34 +32,17 @@ export default function Header() {
 
   return (
     <>
-      <header className="flex items-center justify-between w-full p-4 bg-white shadow-md">
-        <Link href="/dashboard/student">
-          {' '}
-          {/* Or a generic /dashboard if you make one */}
-          {/* <Image
-          src="/RemindED_Logo.png"
-          width={100}
-          height={40}
-          className="w-auto h-10"
-        /> */}
-        </Link>
-
+      <header className="hidden md:flex items-center justify-between p-6 self-stretch bg-white shadow-md">
         {/* Welcome Message */}
         <div className="hidden md:block">
-          {/* Only render this heading if fullName is not an empty string */}
           {fullName && (
-            <h1 className="text-xl font-semibold text-gray-700">
-              Welcome, {fullName}!
+            <h1 className="text-2xl font-semibold font-poppins text-gray-800 leading-8">
+              Welcome,{' '}
+              <span className="bg-linear-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent">
+                {fullName}!
+              </span>
             </h1>
           )}
-        </div>
-
-        {/* Right-side elements */}
-        <div className="flex items-center space-x-4">
-          {/* Profile Picture (from Google) - We can add this later */}
-          {/* <Image ... /> */}
-
-          {/* Hamburger Icon / Nav Menu */}
         </div>
       </header>
     </>
--- app/(pages)/dashboard/layout.jsx
+++ app/(pages)/dashboard/layout.jsx
@@ -6,10 +6,11 @@ import NavigationBar from './_components/NavigationBar';
 export default function DashboardLayout({ children }) {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Header />
       <NavigationBar />
-
-      <main className="p-6 pb-20 md:pb-6 md:pr-64">{children}</main>
+      <main className="md:flex-1 md:ml-64">
+        <Header />
+        <div className="p-6">{children}</div>
+      </main>
     </div>
   );
 }
