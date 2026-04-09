23a907a602b8c9982d058eccf394cfd6f8407479
rammne <rametiongson@gmail.com>
2026-04-10 02:59:33 +0800

    decoupled worker

--- worker/src/ports/ITextExtractorService.ts
+++
@@ -1,7 +0,0 @@
-export interface ITextExtractorService {
-  /**
-   * Extracts plain text from a PDF buffer for downstream chunking/map-reduce.
-   */
-  extractTextFromPdf(buffer: Buffer): Promise<string>;
-}
-
